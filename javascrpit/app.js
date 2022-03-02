a8d80fd4bdf10e3dcfcfa7b3a77561327260a384
sharifulislm <shariful.islam.coder@gmail.com>
2022-03-02 15:53:17 +0300

    first commit

---
+++ javascrpit/app.js
@@ -0,0 +1,29 @@
+const API_KEY = `f66718c08526180c2986933e87a7b79a`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('searchInput').value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayTemperature(data))
+
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp)
+    setInnerText('condition', temperature.weather[0].main)
+    console.log(temperature);
+    //    set weather icon 
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+
+
+}
